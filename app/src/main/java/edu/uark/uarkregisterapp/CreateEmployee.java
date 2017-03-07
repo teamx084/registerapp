d628daefda996b20b749c0419cbc99bc04a1593e
jmarkdixon <jmdixon@uark.edu>
2017-03-07 15:11:28 -0600

    Add files via upload

---
+++ app/src/main/java/edu/uark/uarkregisterapp/CreateEmployee.java
@@ -0,0 +1,18 @@
+package edu.uark.uarkregisterapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import edu.uark.uarkregisterapp.models.transition.ProductTransition;
+
+public class CreateEmployee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__employee__screen);
+    }
+
+}
